0b5601c1c16be2e1209e3b31666b034059f33d86
Nicolep3r <email>
2026-03-03 12:24:17 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+//Ejercicio 1 - Bienvenida
+/*function saludo(){
+    alert("Bienvenido :)");
+}
+saludo();
+
+//Ejercicio 2 
+function calculoMayor(primerNumero, segundoNumero) {
+    if(primerNumero > segundoNumero){
+        alert("El primer numero es mayor que el segundo");
+    }else if(primerNumero == segundoNumero){
+        alert("Los numeros son iguales");
+    }else{
+        alert("El segundo numero es mayor que el primero");
+    }
+}
+ 
+let num1 = 4;
+let num2 = 4;
+calculoMayor(num1, num2);
+
+
+
+
+const dado[1,2,3,4,5,6];
+function tirarDados(arrayDados){
+
+    
+}
+frutas(math.floor(math.random)
+//Ejercicio 3 Volve a intentaflo como lo hicist en la faacu con nueov array
+
+const amigo1 = ["Ryan", "Kieran", "Mark", "Miguel"];
+const amigo2 = ["Ringo", "Jhon", "Paul", "George", "Ada", "Marie"];
+function filtrarAmigos(amigosEnArray) {
+    for(let numero = 0 ; numero < amigosEnArray.length; numero++){
+        if(amigosEnArray[numero].length >= 5 || amigosEnArray[numero].length < 4){
+            amigosEnArray.splice(numero, 1);
+        }
+    }
+    console.log(amigosEnArray);
+}
+filtrarAmigos(amigo2);
+*/
+
+const amigo1 = ["Ryan", "Kieran", "Mark", "Miguel"];
+const amigo2 = ["Ringo", "Jhon", "Paul", "George", "Ada", "Marie"];
+let amigos = [];
+function filtrarAmigos(amigosEnArray) {
+    for(let numero = 0 ; numero < amigosEnArray.length; numero++){
+        if(amigosEnArray[numero].length == 4){
+            //amigosEnArray.splice(numero, 1);
+            amigos.push(amigosEnArray[numero]);
+        }
+    }
+    console.log(amigos);
+}
+filtrarAmigos(amigo2);
+
